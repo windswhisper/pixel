951c00765df34123ef7f8b290fec9e440b7db46e
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-08-29 22:43:07 +0800

    sql

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ http.createServer(function (request, response) {
   fs.readFile(url.slice(1),function (err,data){
                 response.end(data);
             });
-}).listen(18080);
+}).listen(80);
