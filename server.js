e5c59a261a8ef50c7077b0f25e351bf1acff8d96
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-09-16 21:01:45 +0800

    port

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+var http = require('http');
+var fs   = require("fs");
+
+
+
+var httpServer = http.createServer(function (request, response) {
+  //response.writeHead(200, {'Content-Type': 'text/html'});
+  var url = request.url;
+  fs.readFile(url.slice(1),function (err,data){
+                response.end(data);
+            });
+}).listen(80);
+
 var ws = require('ws').Server;
-var server = new ws({port:80});
+var server = new ws({server:httpServer});
 var MainService = require('./MainService.js');
 
 /*
@@ -35,17 +48,3 @@ wss.on( 'connection', function ( wsConnect ) {
 var mainService = MainService.getInstance();
 mainService.init(server);
 
-
-
-var http = require('http');
-var fs   = require("fs");
-
-
-
-http.createServer(function (request, response) {
-  //response.writeHead(200, {'Content-Type': 'text/html'});
-  var url = request.url;
-  fs.readFile(url.slice(1),function (err,data){
-                response.end(data);
-            });
-}).listen(2000);
