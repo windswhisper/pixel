9a918a21fafd901fe96087724e6eb34bb2ba6ac2
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-10-17 23:43:54 +0800

    sqlbug

--- User.js
+++ User.js
@@ -1,4 +1,5 @@
 var querySql = require("./querySql.js");
+var copyFile = require("./copyFile.js");
 var escapeSql = require('./escapeSql');
 
 var EVENT_LOGIN_RES = 2004;
@@ -54,7 +55,12 @@ function User()
       querySql('INSERT INTO pp_painting(width,height,bitmap) SELECT width,height,bitmap FROM pp_painting WHERE id ='+DEFAULT_PAINTING[i],function(err,result,field){
         var id = result.insertId;
         if(id!=null)
-          querySql("INSERT INTO pp_paint(painter_id,painting_id) VALUES("+ws.id+","+id+")");
+        {
+          querySql("INSERT INTO pp_paint(painter_id,painting_id) VALUES("+ws.id+","+id+")",function(err2,result2,field2){
+            var newId = result2.insertId;
+            copyFile(id,newId);
+          });
+        }
       });
     }
   }
