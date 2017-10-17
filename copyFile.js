f0cd12c6b35c294e2e291dba5296fd0c0aaeeb8b
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-10-17 23:59:14 +0800

    sqlbug

--- copyFile.js
+++ copyFile.js
@@ -1,7 +1,7 @@
 var fs=require("fs");
 
 function writeFile(newId,data){
-  fs.writeFile("pic/"+newId+".png",data,'utf8',function(error){
+  fs.writeFile("pic/"+newId+".png",data,function(error){
     if(error){
       throw error;
     }else{
@@ -13,7 +13,7 @@ function writeFile(newId,data){
 function copyFile(id,newId){
   var fs = require('fs');
   console.log("copy"+id+"-"+newId);
-  fs.readFile("pic/"+id+".png", 'utf-8', function(err, data) {
+  fs.readFile("pic/"+id+".png", function(err, data) {
     if (err) {
       console.log("read fail");
     } else {
@@ -23,5 +23,4 @@ function copyFile(id,newId){
   });
 }
  
-
 module.exports = copyFile;
