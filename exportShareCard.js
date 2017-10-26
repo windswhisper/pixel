ad60dada8d111396ca49f77d30b8c4ebdc0fca80
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-10-26 22:43:53 +0800

    share

--- exportShareCard.js
+++ exportShareCard.js
@@ -3,7 +3,7 @@ var images = require('images');
 
 function exportShareCard(id) {
   var FIX_SIZE = 192;
-	var res = images(790,1200).fill(255,255,255);
+	var res = images(790,1061).fill(255,255,255);
   var image = images("pic/"+id+".png").resize(586,586);
   var info = images("pic/info.png");
   res.draw(info,0,0);
